99ab31806f5cbcb439c631cdf77cff508f3bc08f
Brad H. <12530426+BradENGR@users.noreply.github.com>
2024-07-09 15:05:01 -0400

    fix

--- bluetooth.js
+++ bluetooth.js
@@ -1,27 +1,32 @@
 async function discoverDevices(debug = false) {
     try {
         const options = {
-            filters: [{ namePrefix: '' }],
+            filters: [{ namePrefix: 'GEOPH' }],
             optionalServices: ['generic_access'] // Include generic_access to discover all devices
+
+            // Here, we use a simple options object without 'namePrefix'
+            //filters: []
         };
         const deviceList = [];
 
         if (debug) console.log('Requesting devices with options: ', options);
         
-        // Navigator bluetooth scan returns a promise
-        navigator.bluetooth.requestLEScan(options).then(scan => {
-            navigator.bluetooth.addEventListener('advertisementreceived', event => {
-                const device = {
-                    name: event.device.name,
-                    uuids: event.uuids || [],
-                };
-                if (debug) {
-                    console.log('Advertisement received:', device);
-                }
-                deviceList.push(device);
-                displayDevices(deviceList, debug);
-            });
+        const scan = await navigator.bluetooth.requestLEScan(options);
+
+        navigator.bluetooth.addEventListener('advertisementreceived', event => {
+            const device = {
+                name: event.device.name,
+                uuids: event.uuids || [],
+            };
+            if (debug) {
+                console.log('Advertisement received:', device);
+            }
+            deviceList.push(device);
+            displayDevices(deviceList, debug);
         });
+
+        if (debug) console.log('Scanning for advertisements...');
+        
     } catch (error) {
         if (debug) console.error('Error discovering devices: ', error);
         document.getElementById('status').innerText = `Error: ${error.message}`;
